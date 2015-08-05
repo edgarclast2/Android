5ec363b769119c7a6477e914d142f363b09cdb02
pineda <pineda.joomla@gmail.com>
2015-08-05 16:50:40 -0500

    CAMBIANDO FONDO DE LA PANTALLA

---
+++ CAMBIANDO FONDO DE PANTALLA/app/src/androidTest/java/com/example/shey/diccionario_espaol_quechua/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.shey.diccionario_espaol_quechua;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by SHEY on 01/07/2015.
+ */
+public class MainActivityTest extends TestCase {
+
+}
