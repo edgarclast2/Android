7696c00c8ad53553a2b5b2a47a3fcda0f5566491
pineda <pineda.joomla@gmail.com>
2015-08-05 22:40:03 -0500

    avance cuatro de proyecto insertando imagen del cholito

---
+++ AVANCE CUATRO/src/main/java/com/example/edgar/traductor/traductor.java
@@ -0,0 +1,13 @@
+package com.example.edgar.traductor;
+
+import android.os.Bundle;
+
+/**
+ * Created by EDGAR on 28/07/2015.
+ */
+public class traductor extends MainActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.traductor);
+    }
+}
