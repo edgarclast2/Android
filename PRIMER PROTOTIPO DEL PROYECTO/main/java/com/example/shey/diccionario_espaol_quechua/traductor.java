df2e48f4ae32c85b43f387531586ae932d4ab5af
pineda <pineda.joomla@gmail.com>
2015-08-05 20:38:27 -0500

    primer demo del proyecto traductor castellano quechua

---
+++ PRIMER PROTOTIPO DEL PROYECTO/main/java/com/example/shey/diccionario_espaol_quechua/traductor.java
@@ -0,0 +1,19 @@
+package com.example.shey.diccionario_espaol_quechua;
+
+/**
+ * Created by SHEY on 01/07/2015.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+public class traductor  extends MainActivity {
+
+protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.traductor);
+}
+
+}
